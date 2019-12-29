06ccea9a61db33e633bbfe6460b88f432eb70164
“princekrroshan01” <princekrroshan01@gmail.com>
2019-12-29 14:11:40 +0530

    initial commit

---
+++ api/api.py
@@ -0,0 +1,29 @@
+import webbrowser
+from apiclient.discovery import build      
+class API:
+	def __init__(self,keyword,maxlen):
+		self._api_key='AIzaSyBwDMpvqtUxZ6ZsAZ_VxspbuATh-CncRWA'
+		youtube=build('youtube','v3',developerKey=self._api_key)
+		self.keyword=keyword
+		self.maxlen=maxlen
+		req=youtube.search().list(q=self.keyword,part='snippet',type='video',maxResults=self.maxlen)
+		self.result=req.execute() 
+	def open_id(self,item_no):
+		return webbrowser.open('https://www.youtube.com/watch?v='+self.result['items'][item_no]['id']['videoId'])
+	def get_titles(self):
+		stm=[]
+		for i in range(self.maxlen):
+			stm.append(self.result['items'][i]['snippet']['title'])
+		return stm
+	def get_descriptions(self):
+		stm=[]
+		for i in range(self.maxlen):
+			stm.append(self.result['items'][i]['snippet']['description'])
+		return stm
+	def get_image_urls(self):
+		stm=[]
+		for i in range(self.maxlen):
+			stm.append(self.result['items'][0]['snippet']['thumbnails']['medium']['url'])
+		return stm
+		
+		
