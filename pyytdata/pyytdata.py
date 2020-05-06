876827a00ec64d462a9f39e1b7e3691d0dfcc0b2
princekrroshan01 <princekrroshan01@gmail.com>
2020-05-06 09:27:17 +0530

    added readablility to code

--- pyytdata/pyytdata.py
+++ pyytdata/pyytdata.py
@@ -1,44 +1,73 @@
-print("pyytdata imported")
+"""
+Contains a class PyYtData having implementation of youtube data v3 client.
+"""
 import os
+from typing import Any
 
 import webbrowser
 from apiclient.discovery import build
 
 
 class PyYtData:
-    def __init__(self, keyword, maxlen):
-        self._API_KEY = os.environ.get("API_KEY")
-        # link to get the api key is in readme file
-        youtube = build("youtube", "v3", developerKey=self._API_KEY)
+    """
+    Class which acts as a client to the youtube data v3 API,
+    having attributes as the query parameter for API.
+    """
+
+    maxlen: Any
+
+    def __init__(self, keyword, maxlen, order="relevance", type="video"):
+		try:
+        	self.__API_KEY = os.environ.get(
+        	    "API_KEY"
+        	)  # link to get the api key is in readme file
+		except Exception:
+			raise TypeError("You must have API_KEY set as an environment variable")        
+		youtube = build("youtube", "v3", developerKey=self.__API_KEY)
         self.keyword = keyword
         self.maxlen = maxlen
+        self.order = order
+        self.type = type
         req = youtube.search().list(
-            q=self.keyword, part="snippet", type="video", maxResults=self.maxlen
+            q=self.keyword,
+            part="snippet",
+            maxResults=self.maxlen,
+            type=self.type,
+            order=self.order,
         )
         self.result = req.execute()
 
     def open_id(self, item_no):
+        """"Opens the video in default browser of the system."""
         return webbrowser.open(
             "https://www.youtube.com/watch?v="
             + self.result["items"][item_no]["id"]["videoId"]
         )
 
     def get_titles(self):
-        stm = []
+        """"Returns a list with title of the videos."""
+        rslt = []
         for i in range(self.maxlen):
-            stm.append(self.result["items"][i]["snippet"]["title"])
-        return stm
+            rslt.append(self.result["items"][i]["snippet"]["title"])
+        return rslt
 
     def get_descriptions(self):
-        stm = []
+        """"Return list with description of the video."""
+        rslt = []
         for i in range(self.maxlen):
-            stm.append(self.result["items"][i]["snippet"]["description"])
-        return stm
+            rslt.append(self.result["items"][i]["snippet"]["description"])
+        return rslt
 
     def get_image_urls(self):
-        stm = []
-        for i in range(self.maxlen):
-            stm.append(
+
+        """
+        Function to get the image url which will be used for retriving the icon image of
+        the youtube video.
+        """
+
+        rslt = []
+        for _ in range(self.maxlen):
+            rslt.append(
                 self.result["items"][0]["snippet"]["thumbnails"]["medium"]["url"]
             )
-        return stm
+        return rslt
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pyytdata",
+    version="0.0.2",
+    author="Prince Roshan",
+    author_email="princekrroshan01@gmail.com",
+    url="https://github.com/princekrroshan01/youtube-api-wrapper",
+    description=("this is a simple client for youtube data api v3"),
+    long_description=read("README.md"),
+    license="MIT",
+    py_modules=["pyytdata/pyytdata"],
+    install_requires=["google-api-python-client"],
+    include_package_data=True,
+)
