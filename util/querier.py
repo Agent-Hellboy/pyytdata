34637b1a5f343e477cd3eb7f94924919e564ca1b
“princekrroshan01” <princekrroshan01@gmail.com>
2020-09-15 18:44:10 +0530

    fix typo and improved architecture of the package

---
+++ util/querier.py
@@ -0,0 +1,22 @@
+from util.info import Info
+
+
+class VidQuerier(Info):
+    def __init__(self, keyword, maxlen, order="relevance", type="video", videoCategoryId=27):
+        self.keyword = keyword
+        self.maxlen = maxlen
+        self.order = order
+        self.type = type
+        super().__init__(keyword, maxlen, order="relevance", type="video")
+        req = self.youtube.search().list(
+            q=self.keyword,
+            part="snippet",
+            maxResults=self.maxlen,
+            type=self.type,
+            order=self.order,
+            videoCategoryId=videoCategoryId
+        )
+        self.result = req.execute()
+
+    def get_result(self):
+        return self.result
